67514ffa19a5999b2b34fae9c07aa57a204c7685
royanflah12-collab <royanflah12@gmail.com>
2026-08-08 19:15:46 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,6 @@ export const firebaseConfig = {
   apiKey: "AIzaSyCURMLsgC4f3-x5myHIGdwMQJv912QZCX0",
   authDomain: "tema-yearbook.firebaseapp.com",
   databaseURL: https://tema-yearbook-default-rtdb.asia-southeast1.firebasedatabase.app/
-  projectId: "tema-yearbook",
   storageBucket: "tema-yearbook.firebasestorage.app",
   messagingSenderId: "750246371563",
   appId: "1:750246371563:web:808e0880a2dd82a31d1eae",
