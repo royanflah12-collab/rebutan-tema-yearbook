935bf52c7a074702e956926ac89a1a671e9a7131
royanflah12-collab <royanflah12@gmail.com>
2026-08-08 19:21:05 +0700

    Update Firebase configuration with new API key

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCURMLsgC4f3-x5myHIGdwMQJv912QZCX0",
+  apiKey: "API_KEY_KAMU",
   authDomain: "tema-yearbook.firebaseapp.com",
-  databaseURL: https://tema-yearbook-default-rtdb.asia-southeast1.firebasedatabase.app/
+  databaseURL: "https://tema-yearbook-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "tema-yearbook",
   storageBucket: "tema-yearbook.firebasestorage.app",
   messagingSenderId: "750246371563",
   appId: "1:750246371563:web:808e0880a2dd82a31d1eae",
