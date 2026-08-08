df89c49091927c7372e3d736d3f6b736b8452efb
royanflah12-collab <royanflah12@gmail.com>
2026-08-09 00:43:12 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyCURMLsgC4f3-x5myHIGdwMQJv9l2QZCX0",
   authDomain: "tema-yearbook.firebaseapp.com",
   databaseURL: "https://tema-yearbook-default-rtdb.asia-southeast1.firebasedatabase.app",
